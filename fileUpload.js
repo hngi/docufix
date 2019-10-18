c1ebc7b2fc0f18e9233eb4de7f3ca4dc130bb063
Samuel Egbajie <49847624+CodePapi@users.noreply.github.com>
2019-10-18 01:07:59 +0100

    Update fileUpload.js

--- fileUpload.js
+++ fileUpload.js
@@ -1,4 +1,4 @@
-function csrfSafeMethod(method) {
+ function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }

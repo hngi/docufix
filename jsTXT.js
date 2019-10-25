4f41b93121e978f1974bf1bc19a45b6a0f3d5315
CodePapi <49847624+CodePapi@users.noreply.github.com>
2019-10-25 22:55:09 +0100

    .txt file correction

--- jsTXT.js
+++ jsTXT.js
@@ -3,7 +3,7 @@
 
 function txt(){
     
-     var sourceHTML = document.getElementById("textareaDifferences").innerHTML;
+     var sourceHTML = document.getElementById("textareaDifferences").textContent;
      var fileName = document.getElementById('saveAs').value;
      var source = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(sourceHTML);
      var fileDownload = document.createElement("a");
